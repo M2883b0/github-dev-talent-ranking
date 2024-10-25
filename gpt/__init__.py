10198eb2e306c3f801cd8bf2f8740a16197d625e
m2883b0 <2993517036@qq.com>
2024-10-25 21:03:32 +0800

    get topic

---
+++ gpt/__init__.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    :   __init__.py.py    
+@Author  :   m2883b0
+@Modify Time      @Version    @Desciption
+------------      --------    -----------
+2024/10/25 21:02    1.0         None
+"""
