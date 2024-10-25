90f958e376a0da8941f3157a3b5d994d92e99e01
m2883b0 <2993517036@qq.com>
2024-10-25 22:36:21 +0800

    爬虫目录结构优化

--- dataCrawler/main.py
+++ dataCrawler/main.py
@@ -1,33 +1,31 @@
 # -*- encoding: utf-8 -*-
 """
-@File    :   spider.py    
+@File    :   dataCrawler.py
 @Author  :   m2883b0
 @Modify Time      @Version    @Desciption
 ------------      --------    -----------
 2024/10/25 21:07    1.0         None
 """
 
-import scrapy
-import json
 import requests
+from dataCrawler.config import user_info_api_template
 
 
-class MySpider(scrapy.Spider):
+class MySpider:
+    name = "myspider"
+
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        # super().__init__(**kwargs)
+        pass
 
     def spider(self):
         """
 
         :return:
         """
-
-    def get_user(self):
-        """
-
-        :return:
-        """
+        pass
 
 
-if __name__ == "__main__":
-    pass
+
+
+
