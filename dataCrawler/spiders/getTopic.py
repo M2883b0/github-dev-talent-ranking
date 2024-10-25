500d05889a7b7e8dfe6641885c2a2bfb7bdf81af
1906200111 <364715765@qq.com>
2024-10-25 23:59:40 +0800

    get topic

--- dataCrawler/spiders/getTopic.py
+++ dataCrawler/spiders/getTopic.py
@@ -28,13 +28,13 @@ class NewsSpider(scrapy.Spider):
         while True:
             time.sleep(2)  # 等待时间
             sel = Selector(text=self.driver.page_source)
-            titles = sel.css('p.f3.lh-condensed.mb-0.mt-1.Link--primary::text').getall()  # 替换为实际的选择器
+            titles = sel.css('p.f3 lh-condensed mb-0 mt-1 Link--primary::text').getall()  # 替换为topic文本所在的类的名字，作为选择器
 
             for title in titles:
                 yield {'title': title}
 
             try:
-                load_more_button = self.driver.find_element("xpath", "/html/body/div[1]/div[5]/main/div[4]/div[1]/form/button']")  # 替换为实际的按钮文本
+                load_more_button = self.driver.find_element("xpath", "/html/body/div[1]/div[5]/main/div[4]/div[1]/form/button']")  # 替换为实际的按钮文本的xpath
                 load_more_button.click()
             except Exception as e:
                 self.logger.info("没有更多内容了，停止爬取。")
