2b389b3431c90524fa77bf93c58d6f4753b95e4c
1906200111 <364715765@qq.com>
2024-10-27 17:37:03 +0800

    星火大模型测试

---
+++ gpt/config.py
@@ -0,0 +1,19 @@
+# 星火账号信息
+
+# Websocket的方式==============
+# 选择模型
+SPARKAI_URL = 'wss://spark-api.xf-yun.com/v1.1/chat'     #免费的
+SPARKAI_DOMAIN = 'lite'                                  #免费的
+# 账户信息（要更改）
+SPARKAI_APP_ID = '97c319c8'
+SPARKAI_API_SECRET = 'MmM4MWQyNmE4Yjc5OTQ3N2Y1YjgyN2Mx'
+SPARKAI_API_KEY = 'cd82673c0fa28173962de388b84146c9'
+
+
+#http的方式：鉴权(是从调试中心，转代码拿到的) ================
+# 选择模型
+SPARKAI_HTTP_URL = "https://spark-api-open.xf-yun.com/v1/chat/completions"
+# 账户信息（要更改）
+SPARKAI_Authorization = "Bearer pgURxdRsxExPPZsDiqCn:ewVOvLzaHXWzqivyWlJy"
+
+
