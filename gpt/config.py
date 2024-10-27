66b784b2cced60650af72371fbb234b885cf1441
1906200111 <364715765@qq.com>
2024-10-27 22:37:10 +0800

    星火大模型测试

--- gpt/config.py
+++ gpt/config.py
@@ -10,10 +10,9 @@ SPARKAI_API_SECRET = 'MmM4MWQyNmE4Yjc5OTQ3N2Y1YjgyN2Mx'
 SPARKAI_API_KEY = 'cd82673c0fa28173962de388b84146c9'
 
 
-#http的方式：鉴权(是从调试中心，转代码拿到的) ================
+# http的方式：鉴权(是从调试中心，转代码拿到的) ================
 # 选择模型
 SPARKAI_HTTP_URL = "https://spark-api-open.xf-yun.com/v1/chat/completions"
 # 账户信息（要更改）
 SPARKAI_Authorization = "Bearer pgURxdRsxExPPZsDiqCn:ewVOvLzaHXWzqivyWlJy"
-
 
--- gpt/spark_test.py
+++ gpt/spark_test.py
@@ -5,31 +5,30 @@ from sparkai.core.messages import ChatMessage
 import re
 import json
 import requests
-from config import SPARKAI_URL,SPARKAI_APP_ID,SPARKAI_API_SECRET,SPARKAI_API_KEY,SPARKAI_DOMAIN,SPARKAI_Authorization,SPARKAI_HTTP_URL
+from config import SPARKAI_URL, SPARKAI_APP_ID, SPARKAI_API_SECRET, SPARKAI_API_KEY, SPARKAI_DOMAIN, \
+    SPARKAI_Authorization, SPARKAI_HTTP_URL
 
-
-#topic_list从数据库拿（只需要拿一次）
-topic_list = []
+# topic_list从数据库拿（只需要拿一次）
+topic_lists = []
 with open("../topicList.txt", "r") as f:
     t = f.read().strip().split("\n")
-topic_list = list(t)
-print(topic_list)
+topic_lists = list(t)
+print(topic_lists)
 
-#项目的描述，从数据库里拿（对所有没有topic的项目，都需要爬取）
-# project_description = 'huggingface\Transformers: State-of-the-art Machine Learning for Pytorch, TensorFlow, and JAX.'
-project_description = 'Visual Instruction Tuning (LLaVA) built towards GPT-4V level capabilities and beyond.'
-
-
-
+# 项目的描述，从数据库里拿（对所有没有topic的项目，都需要爬取）
+# description = 'huggingface\Transformers: State-of-the-art Machine Learning for Pytorch, TensorFlow, and JAX.'
+description = 'Linux kernel source tree'
+# description = 'Visual Instruction Tuning (LLaVA) built towards GPT-4V level capabilities and beyond.'
 
 
-def output_to_topic(output):
+def output_to_topic(output, topic_list):
     """
 
     :param output:大模型的输出文本
+    :param topic_list:
     :return:
     """
-    #通过正则匹配，提取大模型输出文本的feature tpoic
+    # 通过正则匹配，提取大模型输出文本的feature tpoic
     pattern = r'\b(' + '|'.join(re.escape(tech) for tech in topic_list) + r')\b'
     # 查找大模型输出文本中，提到的技术
     matches = re.findall(pattern, output, flags=re.IGNORECASE)
@@ -44,9 +43,7 @@ def output_to_topic(output):
     return unique_matches
 
 
-
-
-def test_no_stream(topic_list,project_description):
+def websocket_no_stream(topic_list, project_description):
     """
 
     :return:
@@ -62,68 +59,47 @@ def test_no_stream(topic_list,project_description):
     messages = [
         ChatMessage(
             role="system",
-            content='你现在是一名计算机领域的技术顾问，能够准确且简洁的回答用户的问题。请你根据项目文本描述，从技术列表中选出5个最相关的技术元素。'     #ws的话，返回5个即可
+            content='你现在是一名计算机领域的技术顾问，能够准确且简洁的回答用户的问题。'
         ),
         ChatMessage(
             role="user",
-            content='技术名称列表:{}。项目文本描述:"{}"。用列表格式输出：'.format(topic_list, project_description)
+            content='技术名称列表:{}。项目文本描述:"{}"。请你根据项目文本描述，从技术列表中选出10个最相关的技术。用列表格式输出：'.format(topic_list, project_description)
         )
     ]
     handler = ChunkPrintHandler()
     a = spark.generate([messages], callbacks=[handler])
     output = a.generations[0][0].text
     print(output)
-    predict_topic = output_to_topic(output)
-    return predict_topic
+    predict_topic = output_to_topic(output, topic_list)
+    threshold = 4  # 设置给项目最多打4个标签
+    if len(predict_topic) > threshold:
+        return predict_topic[:threshold]  # 给项目上topic，保守一点，最多预测threshold个topic
+    else:
+        return predict_topic
 
 
-# def test_stream():
-#     """
-#     还没修好
-#     :return:
-#     """
-#     from sparkai.core.callbacks import StdOutCallbackHandler
-#     spark = ChatSparkLLM(
-#         spark_api_url=SPARKAI_URL,
-#         spark_app_id=SPARKAI_APP_ID,
-#         spark_api_key=SPARKAI_API_KEY,
-#         spark_api_secret=SPARKAI_API_SECRET,
-#         spark_llm_domain=SPARKAI_DOMAIN,
-#         request_timeout=30, #
-#         streaming=True,
-#
-#     )
-#     messages = [ChatMessage(
-#         role="user",
-#         content='编写一个贪吃蛇游戏的开发流程',
-#         )]
-#     handler = ChunkPrintHandler()
-#     a = spark.generate([messages], callbacks=[handler])
-#     print(a)
-
-
-def http_no_stream(topic_list,project_description):
+def http_no_stream(topic_list, project_description):
     url = SPARKAI_HTTP_URL
     data = {
         "max_tokens": 512,
-        "top_p": 0.6,
-        "top_k": 3,
-        "temperature": 0.3,
-        "presence_penalty": 1,
+        "top_p": 0.8,
+        "top_k": 2,
+        "temperature": 0.5,
+        "presence_penalty": 2,
         "messages": [
             {
                 "role": "system",
-                "content": "你现在是一名计算机领域的技术顾问，能够准确且简洁的回答用户的问题。请你根据项目文本描述，从技术列表中选出10个最相关的技术元素。"    ##http的话，返回10个
+                "content": "你现在是一名计算机领域的技术顾问，能够准确且简洁的回答用户的问题。"
             },
             {
                 "role": "user",
-                "content": "技术名称列表:{}。项目文本描述:{}。用列表格式输出：".format(topic_list, project_description)
+                "content": "把接下来的这句话转换为网址并告诉我这个网址讲了什么：三W点ruanyifeng点com杠blog杠2024杠10杠weekly减issue减321点html"
             }
         ],
         "model": "lite"
     }
     data["stream"] = False
-    #我的鉴权信息
+    # 我的鉴权信息
     header = {
         "Authorization": SPARKAI_Authorization
     }
@@ -137,28 +113,28 @@ def http_no_stream(topic_list,project_description):
     # 非流式:大模型生成完，再一次性返回
     output_json = json.loads(response.text)
 
-    code = output_json['code']  #大模型接口返回的状态码，0表示正确，非0表示出错
+    code = output_json['code']  # 大模型接口返回的状态码，0表示正确，非0表示出错
     if code != 0:
-        print(f'请求错误: {code},{output_json}')        # 接入log   #====
-        return []                                     #大模型非正常输出，返回空     #====
+        print(f'请求错误: {code},{output_json}')  # 接入log   #====
+        return []  # 大模型非正常输出，返回空     #====
     else:
         output = output_json['choices'][0]['message']['content']
         print(output)
-        predict_topic = output_to_topic(output)     #提取大模型的回答内容，提取出符合feature topic list
-        threshold = 4                                  #设置给项目最多打4个标签
+        predict_topic = output_to_topic(output, topic_list)  # 提取大模型的回答内容，提取出符合feature topic list
+        threshold = 4  # 设置给项目最多打4个标签
         if len(predict_topic) > threshold:
-            return predict_topic[:threshold]           #给项目上topic，保守一点，最多预测threshold个topic
+            return predict_topic[:threshold]  # 给项目上topic，保守一点，最多预测threshold个topic
         else:
             return predict_topic
 
 
-
 if __name__ == '__main__':
     # # Websocket的方式
-    # predict_topic = test_no_stream(topic_list, project_description)
+    # predict_topics = websocket_no_stream(topic_lists, description)
 
     # # http的方式
-    predict_topic = http_no_stream(topic_list, project_description)
+    predict_topics = http_no_stream(topic_lists, description)
 
-    #输出结果
-    print(predict_topic)
+
+    # 输出结果
+    print(predict_topics)
